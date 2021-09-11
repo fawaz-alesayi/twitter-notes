aae0350e661bc18da6cc40b1b56b9c9e94eaa606
Fawaz Alesayi <fawaztsa@gmail.com>
2021-09-11 10:36:14 +0300

    change ts-jest for babel

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
   moduleNameMapper: {
     '@/src/(.*)': '<rootDir>/src/$1',
     '@src/(.*)': '<rootDir>/src/$1',
     '@utils/(.*)': '<rootDir>/src/utils/$1',
   },
+  testEnvironment: 'node',
 };
